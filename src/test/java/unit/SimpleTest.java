5dc37b9d6ffc9dd3235897912227858f2727babf
David Julia <dajulia3@gmail.com>
2014-01-27 11:49:21 -0600

    Get a basic, place-holder unit test running

---
+++ src/test/java/unit/SimpleTest.java
@@ -0,0 +1,25 @@
+package unit;
+
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class SimpleTest {
+
+    @BeforeClass
+    public void setUp() {
+    }
+
+    @Test(groups = { "fast" })
+    public void aFastTest() {
+        assertThat(1).isEqualTo(1);
+    }
+
+    @Test(groups = { "slow" })
+    public void aSlowTest() {
+        assertThat("Breakfast").isNotEqualTo("Cookies");
+    }
+
+}
